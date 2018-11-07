41881748675bd4c5cf7cd3c728a28f966ba57e27
Swipel <cp@pietras.dk>
2018-11-07 11:51:41 +0100

    aa

--- AutoFarmApi/Startup.cs
+++ AutoFarmApi/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+//TEST
 namespace AutoFarmApi
 {
     public class Startup
