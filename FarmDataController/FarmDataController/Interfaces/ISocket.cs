fbb02ca9aa6893d6fb798f06d8cba0acb679ed62
Mark Fog Pedersen <TheBigCocio@outlook.dk>
2018-11-08 13:26:28 +0100

    Added LS console app

---
+++ FarmDataController/FarmDataController/Interfaces/ISocket.cs
@@ -0,0 +1,21 @@
+﻿using FarmDataController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmDataController.Interfaces
+{
+    public interface ISocket
+    {
+        IPAddress Ip { get; set; }
+        TcpListener Server { get; set; }
+        TcpClient Client { get; set; }
+        NetworkStream Stream { get; set; }
+        Byte[] IncommingData { get; set; }
+        List<Sensor> Sensors { get; set; }
+    }
+}
---
+++ FarmDataController/FarmDataController/Sockets/MCFeederSocket.cs
@@ -0,0 +1,27 @@
+﻿using FarmDataController.Interfaces;
+using FarmDataController.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmDataController.Sockets
+{
+    class MCFeederSocket : ISocket
+    {
+        public IPAddress Ip { get; set; }
+        public TcpListener Server { get; set; }
+        public TcpClient Client { get; set; }
+        public NetworkStream Stream { get; set; }
+        public Byte[] IncommingData { get; set; }
+        public List<Sensor> Sensors { get; set; }
+
+        public bool FeederFeed()
+        {
+            return true;
+        }
+    }
+}
